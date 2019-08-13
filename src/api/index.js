94436c9a718f6e30bf67a4c8e3c4adf7e1f23c86
wangyibo <wangyibo@innser.sss.com>
2019-08-13 18:59:54 +0800

    调用接口基础路径

--- src/api/index.js
+++ src/api/index.js
@@ -3,14 +3,15 @@
 函数的返回值：promise对象
 */
 import ajax from './ajax'
-const BASE_URL = ''
+const BASE_URL = 'http://localhost:8083'
 //1.根据经纬度获取位置详情
-export const reqAddress = (geohash) => ajax(`/position/${geohash}`)
+// export const reqAddress = (geohash) => ajax(`/position/${geohash}`)
+export const reqAddress = (geohash) => ajax(BASE_URL+'/sell/buyer/product/list')
 //2.获取食品分类列表
 export const reqFoodCategorys = () => ajax('/index_category')
 //3.根据经纬度获取商铺列表
 export const reqShops = (longitude,latitude) => ajax('/shops',{longitude,latitude})
 //4.根据经纬度和关键字搜索商铺列表
-export const reqAddress = () => {}
+// export const reqAddress = () => {}
 //5.获取一次性验证码
-export const reqAddress = () => {}
+// export const reqAddress = () => {}

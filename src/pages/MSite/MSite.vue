2c80aac1ba2a6866ffbce29b21f8198230dd1121
wangyibo <wangyibo@innser.sss.com>
2019-09-09 10:46:03 +0800

    异步显示地址+格式化

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,81 +1,82 @@
 <!--主页-->
 <template>
-    <div class="msite">
-        <!-- 首页头部 -->
-        <header-top title="上海市和平饭店">
-            <span class="header_search" slot="left">
-                <i class="iconfont iconsousuo"></i>
-            </span>
-            <span class="header_login" slot="right">
-                <span class="header_login_text">登录|注册</span>
-            </span>
-        </header-top>
-        <!-- 首页导航 -->
-        <nav class="msite_nav">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/1.jpg">
-                            </div>
-                            <span>三花五罗</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/2.jpg">
-                            </div>
-                            <span>鲤鱼</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/3.jpg">
-                            </div>
-                            <span>鲈鱼</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/4.jpg">
-                            </div>
-                            <span>鲑鱼</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/5.jpg">
-                            </div>
-                            <span>新店特惠</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/6.jpg">
-                            </div>
-                            <span>准时达</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/7.jpg">
-                            </div>
-                            <span>预订早餐</span>
-                        </a>
-                        <a href="javascript:" class="link_to_food">
-                            <div class="food_container">
-                                <img src="./images/nav/8.jpg">
-                            </div>
-                            <span>土豪推荐</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <!-- 首页附近商家 -->
-        <div class="msite_shop_list">
-          <div class="shop_header">
-            <i class="iconfont iconxuanxiang1"></i>
-            <span class="shop_header_title">更多服务</span>
+  <div class="msite">
+    <!-- 首页头部 -->
+    <!-- <header-top :title="address.name"> -->
+    <header-top title="上海市和平饭店">
+      <span class="header_search" slot="left">
+        <i class="iconfont iconsousuo"></i>
+      </span>
+      <span class="header_login" slot="right">
+        <span class="header_login_text">登录|注册</span>
+      </span>
+    </header-top>
+    <!-- 首页导航 -->
+    <nav class="msite_nav">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>三花五罗</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/2.jpg" />
+              </div>
+              <span>鲤鱼</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/3.jpg" />
+              </div>
+              <span>鲈鱼</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/4.jpg" />
+              </div>
+              <span>鲑鱼</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/5.jpg" />
+              </div>
+              <span>新店特惠</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/6.jpg" />
+              </div>
+              <span>准时达</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/7.jpg" />
+              </div>
+              <span>预订早餐</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/8.jpg" />
+              </div>
+              <span>土豪推荐</span>
+            </a>
           </div>
-          <shop-list></shop-list>
         </div>
+      </div>
+    </nav>
+    <!-- 首页附近商家 -->
+    <div class="msite_shop_list">
+      <div class="shop_header">
+        <i class="iconfont iconxuanxiang1"></i>
+        <span class="shop_header_title">更多服务</span>
+      </div>
+      <shop-list></shop-list>
     </div>
+  </div>
 </template>
 
 <script>
@@ -83,72 +84,102 @@ import Swiper from 'swiper'
 import 'swiper/dist/css/swiper.min.css'
 import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
 import ShopList from '../../components/ShopList/ShopList.vue'
+import { mapState } from 'vuex'
 
 export default {
-    mounted() {
-        // 创建一个Swiper实例对象实现轮播
-        new Swiper('.swiper-container',{
-            loop: true, // 可以循环轮播
-            // 如果需要分页器
-            pagination: {
-                el: '.swiper-pagination',
-            }
-        })
-    },
-    components:{HeaderTop,ShopList}
+  components: { HeaderTop, ShopList },
+  mounted() {
+    // 创建一个Swiper实例对象实现轮播
+    new Swiper('.swiper-container', {
+      loop: true, // 可以循环轮播
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      }
+    })
+  },
+  computed: {
+    ...mapState(['address'])
+  },
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  @import "../../common/stylus/mixins.styl"
-    .msite
-        width 100%
-        .msite_nav
-            bottom-border-1px(#e4e4e4)
-            margin-top 15px
-            height 200px
-            background #fff
-            .swiper-container
-                width 100%
-                height 100%
-                .swiper-wrapper
-                    width 100%
-                    height 100%;
-                    margin-top:50px;
-                    .swiper-slide 
-                        display flex
-                        justify-content center
-                        align-items flex-start
-                        flex-wrap wrap
-                        .link_to_food
-                            width 25%
-                            .food_container
-                                display block
-                                width 100%
-                                text-align center
-                                padding-bottom 10px
-                                font-size 0
-                                img
-                                    display inline-block
-                                    width 50px
-                                    height 50px
-                            span
-                                display block
-                                width 100%
-                                text-align center
-                                font-size 13px
-                                color #666
-        .msite_shop_list
-            top-border-1px(#e4e4e4)
-            margin-top 10px
-            background #fff
-            .shop_header
-                padding 10px 10px 0
-                .shop_icon
-                    margin-left 5px
-                    color #999
-                .shop_header_title
-                    color #999
-                    font-size 14px
-                    line-height 20px
+@import '../../common/stylus/mixins.styl';
+
+.msite {
+  width: 100%;
+
+  .msite_nav {
+    bottom-border-1px(#e4e4e4);
+    margin-top: 15px;
+    height: 200px;
+    background: #fff;
+
+    .swiper-container {
+      width: 100%;
+      height: 100%;
+
+      .swiper-wrapper {
+        width: 100%;
+        height: 100%;
+        margin-top: 50px;
+
+        .swiper-slide {
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          flex-wrap: wrap;
+
+          .link_to_food {
+            width: 25%;
+
+            .food_container {
+              display: block;
+              width: 100%;
+              text-align: center;
+              padding-bottom: 10px;
+              font-size: 0;
+
+              img {
+                display: inline-block;
+                width: 50px;
+                height: 50px;
+              }
+            }
+
+            span {
+              display: block;
+              width: 100%;
+              text-align: center;
+              font-size: 13px;
+              color: #666;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .msite_shop_list {
+    top-border-1px(#e4e4e4);
+    margin-top: 10px;
+    background: #fff;
+
+    .shop_header {
+      padding: 10px 10px 0;
+
+      .shop_icon {
+        margin-left: 5px;
+        color: #999;
+      }
+
+      .shop_header_title {
+        color: #999;
+        font-size: 14px;
+        line-height: 20px;
+      }
+    }
+  }
+}
 </style>

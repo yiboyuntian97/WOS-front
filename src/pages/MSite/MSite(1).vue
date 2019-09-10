74cc65476eb9dd990d2848a1b5c037969c9baa86
wangyibo <wangyibo@innser.sss.com>
2019-09-10 13:19:04 +0800

    异步显示主页和商家列表（副本）

--- src/pages/MSite/MSite(1).vue
+++ src/pages/MSite/MSite(1).vue
@@ -13,63 +13,23 @@
     </header-top>
     <!-- 首页导航 -->
     <nav class="msite_nav">
-      <!-- <div class="swiper-container" v-if="category.length"> -->
       <div class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
+          <div class="swiper-slide" v-for="(categorys,index) in categorysArr" :key="index">
+            <a
+              href="javascript:"
+              class="link_to_food"
+              v-for="(category,index) in categorys"
+              :key="index"
+            >
               <div class="food_container">
-                <img src="./images/nav/1.jpg" />
+                <img :src="`${basrImageUrl} + ${category.image_url}`" />
               </div>
-              <span>三花五罗</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/2.jpg" />
-              </div>
-              <span>鲤鱼</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/3.jpg" />
-              </div>
-              <span>鲈鱼</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/4.jpg" />
-              </div>
-              <span>鲑鱼</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/5.jpg" />
-              </div>
-              <span>新店特惠</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/6.jpg" />
-              </div>
-              <span>准时达</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/7.jpg" />
-              </div>
-              <span>预订早餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/8.jpg" />
-              </div>
-              <span>土豪推荐</span>
+              <span>{{category.title}}</span>
             </a>
           </div>
         </div>
       </div>
-      <!-- <img src="./images/nav/msite_back.svg" alt="back" v-else> -->
-      <img src="./images/nav/msite_back.svg" alt="back">
     </nav>
     <!-- 首页附近商家 -->
     <div class="msite_shop_list">
@@ -91,23 +51,62 @@ import { mapState } from 'vuex'
 
 export default {
   components: { HeaderTop, ShopList },
+  data() {
+    return {
+      basrImageUrl: 'https://fuss10.elemecdn.com'
+    }
+  },
+
   mounted() {
-    
+
     this.$store.dispatch('getCategorys')
+    this.$store.dispatch('getShops')
 
-
-    // 创建一个Swiper实例对象实现轮播
-    new Swiper('.swiper-container', {
-      loop: true, // 可以循环轮播
-      // 如果需要分页器
-      pagination: {
-        el: '.swiper-pagination',
-      }
-    })
   },
   computed: {
-    ...mapState(['address'])
+    ...mapState(['address', 'categorys']),
+    /**
+     * 根据categorys一维数组生成一个二维数组
+     * 小数组中的元素个数最大是8
+     */
+    categorysArr() {
+      const { categorys } = this
+      // 准备一个空的二维数组
+      const arr = []
+      // 准备一个小数组(最大长度是8)
+      let minArr = []
+      //遍历categorys
+      categorys.forEach(c => {
+        // 如果当前小数组已经满了，创建一个新的
+        if (minArr.length == 8) {
+          minArr = []
+        }
+        //如果当前minArr是空的，将小数组保存到大数组中
+        if (minArr.length == 0) {
+          arr.push(minArr)
+        }
+        // 将当前分类保存到小数组中
+        minArr.push(c)
+      });
+      return arr
+    },
   },
+  watch: {
+    categorys(value) { // categorys数组中有数据了，在异步更新界面之前执行
+      // 使用setTimeout可以实现效果，但不太好
+      // 希望界面更新就立即创建Swiper对象
+      this.$nextTick(() => { // 一旦完成界面更新立即调用（此条语句要写在数据更新之后）
+        // 创建一个Swiper实例对象实现轮播
+        new Swiper('.swiper-container', {
+          loop: true, // 可以循环轮播
+          // 如果需要分页器
+          pagination: {
+            el: '.swiper-pagination',
+          }
+        })
+      })
+    }
+  }
 }
 </script>
 

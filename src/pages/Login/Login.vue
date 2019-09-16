fb78a9684682375356aacc5e1b34ca782bfa7543
wangyibo <wangyibo@innser.sss.com>
2019-09-16 09:53:27 +0800

    弹出框组件封装+短信登录

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -44,7 +44,8 @@
               </section>
               <section class="login_message">
                 <input type="text" maxlength="11" placeholder="验证码" v-model="captcha"/>
-                <img class="get_verification" src="./images/captcha.svg" alt="captcha" />
+                <!-- <img class="get_verification" src="./images/captcha.svg" alt="captcha" /> -->
+                <img class="get_verification" src="http:localhost:4000/captcha" alt="captcha" @click="getCaptcha"/>
               </section>
             </section>
           </div>
@@ -56,11 +57,15 @@
         <i class="iconfont iconxiazai6"></i>
       </a>
     </div>
+    <AlertTip :alertText="alertText" v-show="alertShow" @closeTip="closeTip"></AlertTip>
   </section>
 </template>
 
 <script>
+import AlertTip from '../../components/AlertTip/AlertTip'
+import {reqSendCode,reqSmsLogin,reqPwdLogin} from '../../api'
 export default {
+  components: {AlertTip},
   data() {
     return {
       loginWay: true, // true代表短信登录，false代表密码登录
@@ -71,34 +76,80 @@ export default {
       name: '', // 用户名
       pwd: '', // 密码
       captcha: '', // 图形验证码
+      alertText: '', // 提示文本
+      alertShow: false, // 是否显示提示框
+      intervalId: 0
     }
   },
   methods: {
+    // 获取一个新的图片验证码
+    getCaptcha(event) {
+      // 每次指定的src值要不同
+      event.target.src="http:localhost:4000/captcha?time="+Date.now()
+    },
     // 异步获取短信验证码
-    getCode() {
+    async getCode() {
       // 如果当前没有计时
       if (!this.computeTime) {
         // 启动倒计时
         this.computeTime = 30
-        const intervalId = setInterval(() => {
+        this.intervalId = setInterval(() => {
           this.computeTime--
           if (this.computeTime <= 0) {
             // 停止计时
-            clearInterval(intervalId)
+            clearInterval(this.intervalId)
           }
         }, 1000)
         // 发送ajax请求（向指定手机号发送验证码短信）
+        const result = await reqSendCode(this.phone)
+        if(result.code == 1) {
+          // 显示提示
+          this.showAlert(result.msg)
+          // 停止倒计时
+          if(this.computeTime) {
+            this.computeTime = 0
+            clearInterval(this.intervalId)
+            this.intervalId = undefined
+          }
+        }
       }
+    },
+    showAlert(alertText) {
+      this.alertShow = true
+      this.alertText = alertText
     },
     login() {
       // 前台表单验证
       if(this.loginWay) { // 短信登录
         const {phone,code} = this
-        console.log(this)
-      }else {
+        // console.log(this)
+        if(!this.rightPhone) {
+          // 手机号不正确
+          // this.showAlert = true
+          // this.alertText = '手机号不正确'
+          this.showAlert('手机号不正确')
+        }else if(!/^\d{6}$/.test(code)) {
+          // 验证码必须是6位数字
+          this.showAlert('验证码必须是6位数字')
+        }
+      }else { // 密码登录
         const {name,pwd,captcha} = this
+        if(!this.name) {
+          // 用户名必须指定
+          this.showAlert('用户名必须指定')
+        }else if (!this.pwd) {
+          // 密码必须指定
+          this.showAlert('密码必须指定')
+        }else if(!this.captcha) {
+          // 验证码必须指定
+          this.showAlert('验证码必须指定')
+        }
       }
     },
+    closeTip() {
+      this.alertShow = false 
+      this.alertText = ''
+    }
   },
   computed: {
     rightPhone() {
